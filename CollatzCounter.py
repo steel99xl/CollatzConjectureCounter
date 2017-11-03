236d556713fb7eae8aae7abe1c36fbd48f7fa56c
steel99xl <steel99xl@gmail.com>
2017-11-03 11:15:15 -0400

    Add files via upload

---
+++ CollatzCounter.py
@@ -0,0 +1,22 @@
+import time
+global x
+global s
+global c
+c = 1
+s = "Line"
+x = c
+print(c)
+while x > 0:
+
+	if x % 2 == 0: #even 
+		x = x/2
+	else: 
+		x = (3*x+1)
+
+	if x == 1:
+		c += 1
+		x = c
+		print("       ")
+		print(c)
+	else:
+		c = c
